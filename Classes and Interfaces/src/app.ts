edf42455ff046f253b39091bb22ee0a5039ebc2b
Abhinav <abhinav7693ngh@gmail.com>
2020-11-23 04:11:13 +0530

    static, abstract

--- Classes and Interfaces/src/app.ts
+++ Classes and Interfaces/src/app.ts
@@ -4,10 +4,14 @@ class Department {
 
     protected employees : string[] = [];
 
+    static financialYear = 2020;
+
     constructor(private readonly name:string){}
 
     describe(this: Department): void{
         console.log("Department is ", this.name);
+        
+        console.log(Department.financialYear); // Accessing financial year that is static.
     }
 
     addEmployee(employee:string){
@@ -16,7 +20,7 @@ class Department {
 
     printEmployees(){
         console.log(this.employees.length);
-        console.log(this.employees);
+        console.log(this.employees); // Here this point to instance as the method is non static.
     }
 
     get getDepartmentName(){
@@ -29,6 +33,10 @@ class Department {
 
     set setEmployeeArray(emp: string[]){
         this.employees = emp;
+    }
+
+    static yo(){
+        console.log(this) // this points to the class not to instance because the method is static.
     }
 }
 
@@ -53,3 +61,38 @@ accounting.setEmployeeArray= ['Abhinav'];
 console.log(accounting.getEmployeeArray);
 
 const accountingCopy = {name : 'Dummy', describe : accounting.describe};
+
+
+
+//  =============== Abstract class example ================== //
+
+abstract class Phone {
+    static entity = "Phone";
+
+    abstract os: string;
+
+    abstract camera(): void;
+
+    powerOn(){
+        console.log("Powers on...");
+    }
+}
+
+class Apple extends Phone{
+
+    constructor(){
+        super();
+    }
+
+    os = "ios";
+
+    camera(){
+        console.log("Best camera :-|");
+
+        this.powerOn(); // Normal inheritance also works
+    }
+}
+
+const pro = new Apple();
+
+pro.camera();
