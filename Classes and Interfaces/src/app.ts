16fdc6428bdf5c0b5bb175ece5b7d826432d6702
Abhinav <abhinav7693ngh@gmail.com>
2020-11-22 22:45:18 +0530

    TS Classes basics

--- Classes and Interfaces/src/app.ts
+++ Classes and Interfaces/src/app.ts
@@ -1,1 +1,30 @@
-// Code goes here!
+class Department {
+    
+    //private readonly name : string;
+
+    private employees : string[] = [];
+
+    constructor(private readonly name:string){}
+
+    describe(this: Department): void{
+        console.log("Department is ", this.name);
+    }
+
+    addEmployee(employee:string){
+        this.employees.push(employee);
+    }
+
+    printEmployees(){
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const accounting = new Department("accounting");
+
+accounting.addEmployee("Abhinav");
+accounting.addEmployee("Arihant");
+
+accounting.printEmployees();
+
+const accountingCopy = {name : 'Dummy', describe : accounting.describe};
